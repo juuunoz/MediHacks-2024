cb19f208972b7e31eb19d913461bcf5ec385a4e6
Annabel <annabel.rch31@gmail.com>
2024-07-09 16:36:26 -0400

    create user class and specialization enum

---
+++ mediup_server/src/classes/specialization.ts
@@ -0,0 +1,9 @@
+export enum Specialization {
+    Doctor = "Doctor",
+    Dentist = "Dentist",
+    Optometrist = "Optometrist",
+    Pediatrician = "Pediatrician",
+    Dermatologist = "Dermatologist",
+  }
+
+// has all the different concentrations/fields. feel free to change this file
---
+++ mediup_server/tests/User.test.ts
@@ -0,0 +1,6 @@
+// User.test.ts
+
+import { User } from '../src/classes/user';
+import { Specialization } from '../src/classes/specialization';
+
+// example test case for get function

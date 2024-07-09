205ad9896b2d9be91febac8bae286c54f5ece8e7
AndrewLin95 <andrew.lin88@hotmail.com>
2024-07-09 17:50:54 -0400

    setup frontend with template pages and auth store

---
+++ mediup_client/src/pages/CreateQuiz/CreateQuiz.tsx
@@ -0,0 +1,11 @@
+const CreateQuiz = () => {
+  return (
+    <>
+      <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
+        <div>CreateQuiz</div>
+      </div>
+    </>
+  );
+};
+
+export default CreateQuiz;
---
+++ mediup_client/src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <>
+      <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
+        <div>Login</div>
+        <button>Click</button>
+      </div>
+    </>
+  );
+};
+
+export default Login;

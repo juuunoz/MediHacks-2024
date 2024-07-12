d10b174761a1a8b8773de4c34bc5a4c8f52a81b7
Annabel <annabel.rch31@gmail.com>
2024-07-12 17:24:26 -0400

    User dealing with Enum values

--- mediup_server/src/schema/authUserSchema.ts
+++ mediup_server/src/schema/authUserSchema.ts
@@ -22,8 +22,10 @@ export const authUserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  // IF THERES AN ERROR ITS PROBABLY HERE
   specialization: {
-    type: Specialization,
+    type: String, // something about mongoose not directly working with enums
+    enum: Object.values(Specialization).map(value => value.toString()), // Allow only enum values
     required: true
   },
   coursesCompleted: {
@@ -52,6 +54,23 @@ export const authUserSchema = new mongoose.Schema({
   }
 })
 
+
+// DEALS WITH ENUM VALUE SPECIALIZATION
+// Set toJSON and toObject transforms to convert enum to string
+authUserSchema.set('toJSON', {
+  transform: function(doc, ret) {
+    ret.specialization = ret.specialization.toString(); // Convert enum value to string
+    return ret;
+  }
+});
+
+authUserSchema.set('toObject', {
+  transform: function(doc, ret) {
+    ret.specialization = ret.specialization.toString(); // Convert enum value to string
+    return ret;
+  }
+});
+
 const AuthUserSchema = mongoose.model('AuthUserSchema', authUserSchema);
 
-module.exports = AuthUserSchema;
+export default AuthUserSchema;

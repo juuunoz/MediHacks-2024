5417de0777c31f0dc55830e63df5be86e30cdd5f
Peanut <p3anuttheboss@gmail.com>
2024-07-12 00:38:17 -0400

    made the signup button functionable to when u click it. will fix visuals later kek

--- mediup_client/src/pages/Login/Login.tsx
+++ mediup_client/src/pages/Login/Login.tsx
@@ -1,40 +1,68 @@
 // src/components/Login.tsx
 
-import React, { CSSProperties } from 'react';
-import Box from './Box'; // Adjust the import path as needed
+import { FC, useState, useEffect } from "react";
 
-const inputStyle: CSSProperties = {
-  paddingLeft: '5px', /* Adjust the padding value as needed */
-  color: 'white', /* Normal text color */
-  borderRadius: '5px', // Rounded corners
-  fontSize: '15px'
-};
+const Login: FC = () => {
+  const [loginToggle, setLoginToggle] = useState(true);
 
-const Login: React.FC = () => {
+  const handleSignupButton = () => {
+    console.log("anna loves peanuts");
+    setLoginToggle(false);
+  };
+
   return (
-    <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
-      
-      <Box 
-        width="30%" 
-        height="40%"
-        backgroundColor="grey" 
-        border="" 
-        padding="20px" 
-        margin="20px"
-        borderRadius= '10px' // Rounded corners
-      >
-        {/* Login title */}
-        <div style={{ textAlign: 'center', marginBottom: '10%', color: 'gold', font: "comic sans",fontSize:'60px' }}>MediUp</div>
-          <div style={{ marginBottom: '10px', textAlign: 'center' }}>
-            {/* Username input */}
-            <input type="username" placeholder="Username or Email" style={inputStyle}/>
-          </div>
-          <div style={{ marginBottom: '20px', textAlign: 'center' }}>
-            {/* Password input */}
-            <input type="password" placeholder="Password" style={inputStyle}/>
-          </div>
-        <button style={{ display: 'block', marginRight: '90%' }}>Login</button>
-      </Box>
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
+      <div className="w-80 h-96 flex flex-col border ">
+        {/* Login title MediUp */}
+        <div
+          className="border"
+          style={{
+            textAlign: "center",
+            marginBottom: "10%",
+            color: "gold",
+            font: "comic sans",
+            fontSize: "60px",
+          }}
+        >
+          MediUp
+        </div>
+        {loginToggle ? (
+          <>
+            <div
+              className="border"
+              style={{ marginBottom: "10px", textAlign: "center" }}
+            >
+              {/* Username input */}
+              <input
+                type="username"
+                placeholder="Username or Email"
+                className="border pl-1 text-blue-700 rounded-md text-md"
+              />
+            </div>
+            <div
+              className="border"
+              style={{ marginBottom: "20px", textAlign: "center" }}
+            >
+              {/* Password input */}
+              <input
+                type="password"
+                placeholder="Password"
+                className="border pl-1 text-blue-700 rounded-md text-md"
+              />
+            </div>
+            <div className="flex flex-row border justify-center pl-5 py-10">
+              <button className="p-8 m-8 " onClick={handleSignupButton}>
+                Sign up
+              </button>
+              <button className="w-24 border">Login</button>
+            </div>
+          </>
+        ) : (
+          <div>signup</div>
+        )}
+
+        {/* </Box> */}
+      </div>
     </div>
   );
 };

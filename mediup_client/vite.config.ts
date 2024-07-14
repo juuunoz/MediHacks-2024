aedc719efc9b1f42e29e587fa786393a4ef78639
Annabel Chao <71451192+Areichao@users.noreply.github.com>
2024-07-14 03:52:46 -0400

    Update vite.config.ts
    added ,

--- mediup_client/vite.config.ts
+++ mediup_client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -17,4 +16,6 @@ export default defineConfig({
   },
   preview: {
     port: parseInt(process.env.PORT) || 3000
+  }
 });
+
